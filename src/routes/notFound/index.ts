c860334b5e1ab9f5d1705182b983cd2fecb4d536
Pedro <82984609+Pedroaaaa@users.noreply.github.com>
2023-04-05 20:18:32 -0300

    Update index.ts

--- src/routes/notFound/index.ts
+++ src/routes/notFound/index.ts
@@ -2,6 +2,6 @@ import { Express, Request, Response } from "../../types/express";
 
 export default (app: Express) => {
   app.route("*").get((req: Request, res: Response) => {
-    res.status(404).json({ message: `${req.originalUrl} does not Fernando` });
+    res.status(404).json({ message: `${req.originalUrl} does not exist` });
   });
 };
