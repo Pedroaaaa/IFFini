e19e09c171281c5a622322b7c12afc3a113c1cca
Pedro <82984609+Pedroaaaa@users.noreply.github.com>
2023-04-08 10:58:09 -0300

    Update index.ts

--- src/routes/notFound/index.ts
+++ src/routes/notFound/index.ts
@@ -2,6 +2,6 @@ import { Express, Request, Response } from "../../types/express";
 
 export default (app: Express) => {
   app.route("*").get((req: Request, res: Response) => {
-    res.status(404).json({ message: `${req.originalUrl} does not exist` });
+    res.status(404).json({ message: `${req.originalUrl} does not exist Jenkins` });
   });
 };
